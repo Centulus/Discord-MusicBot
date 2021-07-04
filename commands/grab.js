9d5737532025d266c08bd4168c32c4738907c860
Centulus <86899297+Centulus@users.noreply.github.com>
2021-07-04 05:03:37 +0200

    Update grab.js

--- commands/grab.js
+++ commands/grab.js
@@ -3,7 +3,7 @@ const prettyMilliseconds = require("pretty-ms");
 
 module.exports = {
   name: "grab",
-  description: "Saves the current song to your Direct Messages",
+  description: "Enregistre la chanson en cours dans vos messages directs (DM)",
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
@@ -19,30 +19,30 @@ module.exports = {
 */
 run: async (client, message, args, { GuildDB }) => {
   let player = await client.Manager.get(message.guild.id);
-  if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
-  if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to play something!**");
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **You must be in the same voice channel as me to use this command!**");
+  if (!player) return client.sendTime(message.channel, "❌ | **Rien n'est joué actuellement...**");
+  if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Vous devez être dans un salon vocal pour jouer quelque chose !**");
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande !**");
    message.author.send(new MessageEmbed()
-   .setAuthor(`Song saved`, client.user.displayAvatarURL({
+   .setAuthor(`Chanson sauvegardée`, client.user.displayAvatarURL({
     dynamic: true
   }))
   .setThumbnail(`https://img.youtube.com/vi/${player.queue.current.identifier}/mqdefault.jpg`)
   .setURL(player.queue.current.uri)
   .setColor("RANDOM")
   .setTitle(`**${player.queue.current.title}**`)
-  .addField(`⌛ Duration: `, `\`${prettyMilliseconds(player.queue.current.duration, {colonNotation: true})}\``, true)
-  .addField(`🎵 Author: `, `\`${player.queue.current.author}\``, true)
-  .addField(`▶ Play it:`, `\`${GuildDB ? GuildDB.prefix : client.config.DefaultPrefix
+  .addField(`⌛ Durée: `, `\`${prettyMilliseconds(player.queue.current.duration, {colonNotation: true})}\``, true)
+  .addField(`🎵 Auteur: `, `\`${player.queue.current.author}\``, true)
+  .addField(`▶ Jouez-le:`, `\`${GuildDB ? GuildDB.prefix : client.config.DefaultPrefix
   }play ${player.queue.current.uri}\``)
-  .addField(`🔎 Saved in:`, `<#${message.channel.id}>`)
-  .setFooter(`Requested by: ${player.queue.current.requester.tag}`, player.queue.current.requester.displayAvatarURL({
+  .addField(`🔎 Sauvegardée dans:`, `<#${message.channel.id}>`)
+  .setFooter(`Demandé par: ${player.queue.current.requester.tag}`, player.queue.current.requester.displayAvatarURL({
     dynamic: true
   }))
     ).catch(e=>{
-      return message.channel.send("**:x: Your DMs are disabled**")
+      return message.channel.send("**:x: Vos DMs sont désactivés**")
     })    
 
-    client.sendTime(message.channel, "✅ | **Check your DMs!**")
+    client.sendTime(message.channel, "✅ | **Vérifiez vos DMs !**")
   },
   SlashCommand: {
 /**
@@ -57,31 +57,31 @@ run: async (client, message, args, { GuildDB }) => {
     const user = client.users.cache.get(interaction.member.user.id);
     const member = guild.members.cache.get(interaction.member.user.id);
     let player = await client.Manager.get(interaction.guild_id);
-    if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**");
-    if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-    if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **You must be in the same voice channel as me to use this command!**");
+    if (!player) return client.sendTime(interaction, "❌ | **Rien n'est joué actuellement**");
+    if (!member.voice.channel) return client.sendTime(interaction, "❌ | **Vous devez être dans un salon vocal pour utiliser cette commande.**");
+    if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande !**");
     try{
     let embed = new MessageEmbed()
-      .setAuthor(`Song saved: `, client.user.displayAvatarURL())
+      .setAuthor(`Chanson sauvegardée: `, client.user.displayAvatarURL())
       .setThumbnail(`https://img.youtube.com/vi/${player.queue.current.identifier}/mqdefault.jpg`)
       .setURL(player.queue.current.uri)
       .setColor("RANDOM")
       .setTimestamp()
       .setTitle(`**${player.queue.current.title}**`)
-      .addField(`⌛ Duration: `, `\`${prettyMilliseconds(player.queue.current.duration, {colonNotation: true})}\``, true)
-      .addField(`🎵 Author: `, `\`${player.queue.current.author}\``, true)
-      .addField(`▶ Play it:`, `\`${GuildDB ? GuildDB.prefix : client.config.DefaultPrefix
+      .addField(`⌛ Durée: `, `\`${prettyMilliseconds(player.queue.current.duration, {colonNotation: true})}\``, true)
+      .addField(`🎵 Auteur: `, `\`${player.queue.current.author}\``, true)
+      .addField(`▶ Jouez-le:`, `\`${GuildDB ? GuildDB.prefix : client.config.DefaultPrefix
         }play ${player.queue.current.uri}\``)
-      .addField(`🔎 Saved in:`, `<#${interaction.channel_id}>`)
-      .setFooter(`Requested by: ${player.queue.current.requester.tag}`, player.queue.current.requester.displayAvatarURL({
+      .addField(`🔎 Sauvegardée dans:`, `<#${interaction.channel_id}>`)
+      .setFooter(`Demandé par: ${player.queue.current.requester.tag}`, player.queue.current.requester.displayAvatarURL({
         dynamic: true
       }))
       user.send(embed);
     }catch(e) {
-      return client.sendTime(interaction, "**:x: Your DMs are disabled**")
+      return client.sendTime(interaction, "**:x: Vos DMs sont désactivés**")
     }
 
-    client.sendTime(interaction, "✅ | **Check your DMs!**")
+    client.sendTime(interaction, "✅ | **Vérifiez vos DMs!**")
   },
   },
 };
