75261b28ce2c3f137b1fa1f9fead9356600fde0c
Centulus <86899297+Centulus@users.noreply.github.com>
2021-07-04 06:45:26 +0200

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -19,9 +19,9 @@ module.exports = {
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
         if (!player) return client.sendTime(message.channel, "❌ | **Rien n'est joué actuellement...**");
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Vous devez être dans un salon vocal pour utiliser cette commande.!**");
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande.!**");
-        if (!player.queue || !player.queue.length || player.queue.length === 0) return client.sendTime(message.channel, "❌ | **Il n'y a pas assez de chansons dans la playlist pour la mélanger.!**");
+        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Vous devez être dans un salon vocal pour utiliser cette commande !**");
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande !**");
+        if (!player.queue || !player.queue.length || player.queue.length === 0) return client.sendTime(message.channel, "❌ | **Il n'y a pas assez de chansons dans la playlist pour la mélanger !**");
         player.queue.shuffle();
         await client.sendTime(message.channel, "✅ | Playlist mélangée!");
     },
@@ -38,7 +38,7 @@ module.exports = {
             const member = guild.members.cache.get(interaction.member.user.id);
 
             if (!member.voice.channel) return client.sendTime(interaction, "❌ | **Vous devez être dans un salon vocal pour utiliser cette commande..**");
-            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande.!**");
+            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Vous devez être dans le même salon vocal que moi pour utiliser cette commande !**");
 
             let player = await client.Manager.get(interaction.guild_id);
             if (!player) return client.sendTime(interaction.channel, "❌ | **Rien n'est joué actuellement...**");
